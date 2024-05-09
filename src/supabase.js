f2aa71939b915892908ab62a713ae0b004b962b3
rhorhodynamite <162373331+rhorhodynamite@users.noreply.github.com>
2024-05-09 17:18:17 +0200

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,9 @@
 // src/supabaseCLIENT.js
 import { createClient } from '@supabase/supabase-js';
 
+console.log("Supabase URL:", process.env.REACT_APP_SUPABASE_URL);
+console.log("Supabase Key:", process.env.REACT_APP_SUPABASE_ANON_KEY);
+
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
