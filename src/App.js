bc2b97817fabb8f22828a90bf350e310f9f5ef55
rhorhodynamite <162373331+rhorhodynamite@users.noreply.github.com>
2024-05-09 12:47:33 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import { useContext } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Login from './otherPages/login/Login';
-import Register from './otherPages/login/Register';
+import RegistrationForm from './RegistrationForm';
 import NoPage from './otherPages/NoPage';
 import Dashboard from './components/Dashboard';
 import Test from './components/Test';
@@ -18,7 +18,7 @@ function App() {
 				<Routes>
 					{/* <Route path="/" exact element={<Register />} /> */}
 					<Route path="/login" element={token ? <Navigate to="/dashboard"/> :<Login />} />
-					<Route path="/register" element={token ? <Navigate to="/dashboard"/> :<Register />} />
+					<Route path="/register" element={token ? <Navigate to="/dashboard"/> :<RegistrationForm />} />
 					<Route path="/dashboard" element={token ? <Dashboard /> :<Navigate to="/login" />} />
 					<Route path="/test" element={<Test/>} />
 					<Route path="/" element={!token ? <Navigate to="/login" /> : <Navigate to="/dashboard"/>} />
