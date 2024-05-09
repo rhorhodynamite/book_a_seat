2b9fcf5ae4a4d1ef73e73000f96a8bbb0e7fd138
rhorhodynamite <162373331+rhorhodynamite@users.noreply.github.com>
2024-05-09 18:00:16 +0200

    Update Login.js

--- src/otherPages/login/Login.js
+++ src/otherPages/login/Login.js
@@ -14,7 +14,7 @@ const Login = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         console.log(supabase.auth);  // Check if auth object is correct
-        const { user, error } = await supabase.auth.SignInWithPassword({
+        const { user, error } = await supabase.auth.signIn({
             email: username,
             password: pwd,
         });
