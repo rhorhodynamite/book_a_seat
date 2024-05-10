515a845518234e4b692b097b39ce1773647f9b99
rhorhodynamite <162373331+rhorhodynamite@users.noreply.github.com>
2024-05-10 14:41:01 +0200

    Update SvgPlan.js

--- src/components/SvgPlan.js
+++ src/components/SvgPlan.js
@@ -13,8 +13,8 @@ function SVGPlan(props) {
           <rect x="150" y="100" width="10" height="10" fill="brown" />
           {/* Plants below Pillar 2 */}
           <rect x="153" y="113" width="5" height="4" fill="green" />
-          <rect x="155" y="125" width="6" height="4" fill="green" />
-          <rect x="154" y="130" width="4" height="3" fill="green" />
+          <rect x="155" y="122" width="6" height="4" fill="green" />
+          <rect x="154" y="129" width="4" height="3" fill="green" />
           {/* Door at the bottom of Room 1 */}
           <rect x="125" y="134" width="20" height="5" fill="brown" />
           {/* Side door at Room 1 */}
