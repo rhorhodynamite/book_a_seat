6765887406467f41876f1dde91bbf2ebf4b8a043
rhorhodynamite <162373331+rhorhodynamite@users.noreply.github.com>
2024-05-10 10:43:46 +0200

    Update SvgPlan.js

--- src/components/SvgPlan.js
+++ src/components/SvgPlan.js
@@ -1,35 +1,16 @@
-
-function SVGPlan(props) {   
+function SVGPlan(props) {
   return (
     <svg width={props.width} height={props.height} version="1.1" viewBox="0 0 175 125" xmlns="http://www.w3.org/2000/svg">
-    <g transform="translate(70.8 -14.7)">
-      <g fill="none" stroke="#000">
-      <path d="m-68.8 66.5v-49.8h171v121h-64" stopColor="#000000" strokeWidth="4.05"/>
-      <path d="m-68.8 87.8v49.8h85.3" stopColor="#000000" strokeWidth="4.05"/>
-      <g strokeWidth="1.9">
-        <path d="m-68.8 52.3h21.3"/>
-        <path d="m-26.1 52.3h14.2v-35.6"/>
-        <path d="m-11.9 52.3h14.2v14.2"/>
-        <path d="m2.32 87.8v21.3h-28.4"/>
-        <path d="m-47.5 109h-21.3"/>
-        <path d="m2.32 109v28.4"/>
-        <path d="m2.32 52.3h14.2"/>
-        <path d="m37.9 52.3h14.2v-35.6"/>
-        <path d="m52.1 52.3v14.2"/>
-        <path d="m102 87.8h-14.2"/>
-        <path d="m66.3 87.8h-14.2v49.8"/>
+      <g transform="translate(70.8 -14.7)">
+        <g fill="none" stroke="#000">
+          {/* Room Outline */}
+          <rect x="-68.8" y="16.7" width="170" height="120" stroke="#000" strokeWidth="2" />
+          {/* Pillar 1 */}
+          <rect x="-18.8" y="100" width="10" height="10" fill="#000" />
+          {/* Pillar 2 */}
+          <rect x="18.8" y="100" width="10" height="10" fill="#000" />
+        </g>
       </g>
-      </g>
-      <g fillOpacity="0">
-      <rect x="-68.8" y="16.7" width="56.9" height="35.6" />
-      <rect x="-11.9" y="16.7" width="64" height="35.6" />
-      <rect x="52.1" y="16.7" width="49.8" height="71.1" />
-      <rect x="-68.8" y="52.3" width="71.1" height="56.9" />
-      <rect x="2.32" y="52.3" width="49.8" height="85.3" />
-      <rect x="52.1" y="87.8" width="49.8" height="49.8" />
-      <rect x="-68.8" y="109" width="71.1" height="28.4" />
-      </g>
-    </g>
     </svg>
   );
 }
