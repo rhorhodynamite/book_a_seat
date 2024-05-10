e205e57ba668d13c51e2c9c1134daccaa835d201
rhorhodynamite <162373331+rhorhodynamite@users.noreply.github.com>
2024-05-10 12:22:22 +0200

    Update SvgPlan.js

--- src/components/SvgPlan.js
+++ src/components/SvgPlan.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 function SVGPlan(props) {
   return (
     <svg width={props.width} height={props.height} version="1.1" viewBox="0 0 180 125" xmlns="http://www.w3.org/2000/svg">
@@ -6,14 +8,13 @@ function SVGPlan(props) {
           {/* Room 1 Outline */}
           <rect x="10" y="16.7" width="170" height="120" stroke="#000" strokeWidth="2" />
           {/* Pillar 1 in Room 1 (slightly moved to the right) */}
-          <rect x="50" y="100" width="10" height="10" fill="#000" />
+          <rect x="50" y="100" width="10" height="10" fill="brown" />
           {/* Pillar 2 in Room 1 (significantly moved to the right) */}
-          <rect x="150" y="100" width="10" height="10" fill="#000" />
+          <rect x="150" y="100" width="10" height="10" fill="brown" />
           {/* Door at the bottom of Room 1 */}
-          <rect x="125" y="134" width="20" height="5" fill="#000" />
+          <rect x="125" y="134" width="20" height="5" fill="brown" />
           {/* Door at the bottom of Room 1 */}
-          <rect x="179" y="60" width="5" height="20" fill="#000" />
-          
+          <rect x="179" y="60" width="5" height="20" fill="brown" />
         </g>
       </g>
     </svg>
