cd3437c3182c76f0f2d8adb15573eb8a136fa45b
rhorhodynamite <162373331+rhorhodynamite@users.noreply.github.com>
2024-05-13 11:46:45 +0200

    Create roomlist.js

---
+++ src/components/roomlist.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function RoomList({ rooms, onSelectRoom }) {
+    return (
+        <div className="room-list">
+            {rooms.map(room => (
+                <div key={room.id} className="room-item" onClick={() => onSelectRoom(room)}>
+                    <h3>{room.name}</h3>
+                    <p>{room.description}</p>
+                </div>
+            ))}
+        </div>
+    );
+}
