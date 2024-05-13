e02d8be72bd4fca4d648c0c1cb2af7c6101d3fb9
rhorhodynamite <162373331+rhorhodynamite@users.noreply.github.com>
2024-05-13 11:48:20 +0200

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -46,7 +46,7 @@ function NavBar() {
   const [keyDiagram, setKeyDiagram] = useState((token.role === 'admin'?1:0))
   const [selSeat, setSelSeat] = useState(null)
 
-  import RoomList from './RoomList';  // Assuming RoomList is a separate component
+
 
   const [keyUpstairsDiagram, setKeyUpstairsDiagram] = useState(1);
   const [selectedRoom, setSelectedRoom] = useState(null);
