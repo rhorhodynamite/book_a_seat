b6a0b451eae5265b14ec465108b695983662f70a
rhorhodynamite <162373331+rhorhodynamite@users.noreply.github.com>
2024-05-13 16:17:59 +0200

    Update SvgPlanUpstairs.js

--- src/components/SvgPlanUpstairs.js
+++ src/components/SvgPlanUpstairs.js
@@ -23,7 +23,7 @@ function SVGPlanUpstairs(props) {
         {/* New line from half way across x from the right side and a quarter way down y */}
         <line x1="250" y1="0" x2="250" y2="150" stroke="black" strokeWidth="2" />
         <line x1="200" y1="50" x2="200" y2="150" stroke="black" strokeWidth="2" />
-        <line x1="150" y1="75" x2="150" y2="100" stroke="black" strokeWidth="2" />
+        <line x1="150" y1="75" x2="100" y2="150" stroke="black" strokeWidth="2" />
       </g>
     </svg>
   );
