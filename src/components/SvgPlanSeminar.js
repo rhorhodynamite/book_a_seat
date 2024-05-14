d82e8642369abb6dfb4ee449cf40075930aa4f3c
rhorhodynamite <162373331+rhorhodynamite@users.noreply.github.com>
2024-05-14 12:49:37 +0200

    Update SvgPlanSeminar.js

--- src/components/SvgPlanSeminar.js
+++ src/components/SvgPlanSeminar.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 
 function SVGPlanSeminar(props) {
@@ -8,14 +7,29 @@ function SVGPlanSeminar(props) {
         <g fill="none" stroke="#000">
           {/* Room 1 Outline */}
           <rect x="10" y="16.7" width="170" height="120" stroke="#000" strokeWidth="2" />
+          
           {/* Pillar 1 in Room 1 (slightly moved to the right) */}
           <rect x="120" y="80" width="20" height="30" fill="brown" />
           
-         
           {/* Door at the bottom of Room 1 */}
           <rect x="125" y="134" width="20" height="5" fill="brown" />
+          
           {/* Side door at Room 1 */}
           <rect x="177" y="60" width="5" height="20" fill="brown" />
+          
+          {/* Corridor coming out of the right door */}
+          <rect x="182" y="60" width="30" height="20" stroke="#000" strokeWidth="2" />
+          
+          {/* Connecting passage space (turns up) */}
+          <rect x="212" y="30" width="20" height="50" stroke="#000" strokeWidth="2" />
+          
+          {/* Staircase 1 along the corridor */}
+          <line x1="192" y1="65" x2="202" y2="65" stroke="black" strokeWidth="2" />
+          <line x1="194" y1="70" x2="204" y2="70" stroke="black" strokeWidth="2" />
+          
+          {/* Staircase 2 along the corridor */}
+          <line x1="192" y1="75" x2="202" y2="75" stroke="black" strokeWidth="2" />
+          <line x1="194" y1="80" x2="204" y2="80" stroke="black" strokeWidth="2" />
         </g>
       </g>
     </svg>
@@ -23,3 +37,4 @@ function SVGPlanSeminar(props) {
 }
 
 export default SVGPlanSeminar;
+
