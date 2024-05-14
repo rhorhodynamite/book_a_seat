5f1a0e5540ec0c16e5ca366e211639ce25985bce
rhorhodynamite <162373331+rhorhodynamite@users.noreply.github.com>
2024-05-14 13:03:24 +0200

    Update SvgPlanUpstairs.js

--- src/components/SvgPlanUpstairs.js
+++ src/components/SvgPlanUpstairs.js
@@ -8,35 +8,36 @@ function SVGPlanUpstairs(props) {
   return (
     <svg width={width} height={height} viewBox="0 0 640 300" xmlns="http://www.w3.org/2000/svg">
       <g fill="none" stroke="#000">
+        {/* Room 4 (smaller, diagonal room on the very left) */}
+        <polygon points="0,50 50,100 100,50 100,200 0,200" stroke="#000" strokeWidth="2" fill="none" />
+        {/* Door to Room 4 from the corridor */}
+        <rect x="50" y="50" width="20" height="6" fill="brown" />
+
         {/* Room 1 */}
-        <rect x="0" y="50" width="200" height="150" stroke="#000" strokeWidth="2" />
+        <rect x="106" y="50" width="200" height="150" stroke="#000" strokeWidth="2" />
         {/* Door between Room 1 and Room 2 */}
-        <rect x="200" y="120" width="6" height="20" fill="brown" />
+        <rect x="306" y="120" width="6" height="20" fill="brown" />
 
         {/* Room 2 */}
-        <rect x="206" y="50" width="200" height="150" stroke="#000" strokeWidth="2" />
+        <rect x="312" y="50" width="200" height="150" stroke="#000" strokeWidth="2" />
         {/* Door between Room 2 and Room 3 */}
-        <rect x="406" y="120" width="6" height="20" fill="brown" />
+        <rect x="512" y="120" width="6" height="20" fill="brown" />
 
         {/* Room 3 (taller) */}
-        <rect x="412" y="50" width="200" height="200" stroke="#000" strokeWidth="2" />
+        <rect x="518" y="50" width="200" height="200" stroke="#000" strokeWidth="2" />
         {/* Door to Corridor */}
-        <rect x="490" y="50" width="20" height="6" fill="brown" />
+        <rect x="590" y="50" width="20" height="6" fill="brown" />
 
-        {/* Corridor connecting Room 3 to Rooms 1 and 2 */}
-        <rect x="490" y="0" width="20" height="50" stroke="#000" strokeWidth="2" />
-        <rect x="0" y="0" width="510" height="50" stroke="#000" strokeWidth="2" />
-        
-        {/* Room 4 (smaller, diagonal room accessible from Room 1) */}
-        <polygon points="200,50 250,100 300,50 300,200 200,200" stroke="#000" strokeWidth="2" fill="none" />
-        {/* Door to Room 4 from Room 1 */}
-        <rect x="250" y="50" width="20" height="6" fill="brown" />
+        {/* Corridor starting from the taller room */}
+        <rect x="590" y="0" width="20" height="50" stroke="#000" strokeWidth="2" />
+        <rect x="0" y="0" width="610" height="50" stroke="#000" strokeWidth="2" />
       </g>
     </svg>
   );
 }
 
 export default SVGPlanUpstairs;
+
 
 
 
