92573e3297b22168164f651c0f1001315b44a86a
rhorhodynamite <162373331+rhorhodynamite@users.noreply.github.com>
2024-05-16 10:49:36 +0200

    Update ReservationList.js

--- src/components/ReservationList.js
+++ src/components/ReservationList.js
@@ -23,228 +23,189 @@ const ElementStyle = styled.div`
 
 const currentDate = moment(new Date()).startOf('day').toDate();
 
-export default function MyBooking(props) {
-  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
-  const [reservationList, setReservationList] = useState([]);
-  const [reservationData, setReservation] = useState([]);
+function ReservationList(props) {
+  const [isCalendarActive, setCalendarActive] = useState(false);
+  const [selRow, setSelRow] = useState(null);
+  const [childKey, setChildKey] = useState(0);
   const [idToDel, setIdToDel] = useState(null);
   const [showAlert, setShowAlert] = useState(null);
+  const [showAlert2, setShowAlert2] = useState(null);
+  const { token } = useContext(AuthContext);
 
-  function loadData() {
-    console.log('load booking');
-
-    const params = {
-      id: props.username,
-    };
-    const loadRequest = async () => {
-      try {
-        const response = await axios.get(
-          GET_URL,
-          { params: params },
-          {
-            withCredentials: true,
-          }
-        );
-        const rslt = response.data?.rslt.map((val, key) => {
-          val.startDate = moment(val.startDate, 'YYYY-MM-DD HH:mm:ss').toDate();
-          val.endDate = moment(val.endDate, 'YYYY-MM-DD HH:mm:ss').toDate();
-          
-          val.mmtS = moment(val.startDate);
-          val.startHour = parseFloat(val.mmtS.format('HH')) + parseFloat(val.mmtS.format('mm') / 60);
-          val.startDay = parseInt(val.mmtS.format('D'));
-          val.startMonth = parseInt(val.mmtS.format('M'));
-          val.startYear = parseInt(val.mmtS.format('YYYY'));
-          val.weekday = val.mmtS.format('ddd');
-          
-          val.mmtE = moment(val.endDate);
-          val.endHour = parseInt(val.mmtE.format('HH')) + parseFloat(val.mmtE.format('mm') / 60);
-          val.endDay = parseInt(val.mmtE.format('D'));
-          val.endMonth = parseInt(val.mmtE.format('M'));
-          val.endYear = parseInt(val.mmtE.format('YYYY'));
-          val.weekday = val.mmtS.format('ddd');
-          return val;
-        });
-
-        const finalMap = [];
-        rslt.forEach((item) => {
-          let monthYearItem = finalMap.find(
-            (item2) => item2.year === item.startYear && item2.month === item.startMonth
-          );
-          if (!monthYearItem) {
-            if (item.startDay === item.endDay) {
-              monthYearItem = {
-                year: item.startYear,
-                month: item.startMonth,
-                days: [
-                  {
-                    day: item.startDay,
-                    dayBook: [{ id: item.id, seatId: item.seatId, from: item.startHour, to: item.endHour }],
-                  },
-                ],
-              };
-              finalMap.push(monthYearItem);
-            } else {
-              addMultiDays(item, finalMap);
-            }
-          } else {
-            const dayItem = monthYearItem.days.find((item3) => item3.day === item.startDay);
-            if (!dayItem) {
-              if (item.startDay === item.endDay) {
-                monthYearItem.days.push({
-                  day: item.startDay,
-                  dayBook: [{ id: item.id, seatId: item.seatId, from: item.startHour, to: item.endHour }],
-                });
-              } else {
-                addMultiDays(item, finalMap);
-              }
-            } else {
-              dayItem.dayBook.push({ id: item.id, seatId: item.seatId, from: item.startHour, to: item.endHour });
-            }
-          }
-        });
-        console.log('modify rslt', rslt);
-        console.log('finalMap', finalMap);
-        setReservation(finalMap);
-        setReservationList(rslt);
-      } catch (err) {
-        console.log('ERROR loadData', err);
-      }
-    };
-    loadRequest();
-  }
-
-  function addMultiDays(item, finalMap) {
-    const currDate = item.mmtS.startOf('day');
-    const lastDate = item.mmtE.startOf('day');
-    let startHour = null;
-    while (currDate.diff(lastDate) <= 0) {
-      const day = parseInt(currDate.format('D'));
-      const weekday = currDate.format('ddd');
-      const month = parseInt(currDate.format('M'));
-      const year = parseInt(currDate.format('YYYY'));
-      startHour = startHour === null ? item.startHour : 0;
-      const endHour = currDate.isSame(lastDate) ? item.endHour : 24;
-
-      let monthYearItem = finalMap.find((item2) => item2.year === year && item2.month === month);
-      if (!monthYearItem) {
-        monthYearItem = {
-          year: year,
-          month: month,
-          days: [
-            {
-              day,
-              weekday,
-              dayBook: [{ id: item.id, seatId: item.seatId, from: startHour, to: endHour }],
-            },
-          ],
-        };
-        finalMap.push(monthYearItem);
-      } else {
-        const dayItem = monthYearItem.days.find((item3) => item3.day === day);
-        if (!dayItem) {
-          monthYearItem.days.push({ day, weekday, dayBook: [{ id: item.id, seatId: item.seatId, from: startHour, to: endHour }] });
-        } else {
-          dayItem.dayBook.push({ id: item.id, seatId: item.seatId, from: startHour, to: endHour });
-        }
-      }
-      currDate.add(1, 'days');
-    }
-  }
+  const [reservationData, setReservation] = useState([]);
 
   useEffect(() => {
-    loadData();
-  }, []);
+    if(props.selSeat)
+      loadData(props.selSeat);
+  }, [props.selSeat]);
 
-  const barEl = (dayBook, dayMonthKey) => {
-    return dayBook.map((val, key) => {
-      const colorId4SeatId = val.seatId % 4;
-      const widthBar = (val.to - val.from) / 24 * CONTENT_WIDTH;
-      const left = val.from / 24 * CONTENT_WIDTH;
-      const style = { width: widthBar + 'px', left: left + 'px', backgroundColor: COLORS[colorId4SeatId] };
-      const fTDayMonthKey = val.from + '_' + val.to + '_' + dayMonthKey;
-      const r = reservationList.find((item2) => val.id === item2.id);
-      const fTime = r.mmtE.format('HH:mm');
-      const txtContent = `from  ${r.mmtS.format('HH:mm')} to ${fTime === '00:00' ? '24:00' : fTime}, desk id: ${val.seatId}`;
-      const titleContent = `from ${r.mmtS.format('DD.MM.yyyy HH:mm')} to ${r.mmtE.add(-1, 'ss').format('DD.MM.yyyy HH:mm')}, desk id: ${val.seatId}`;
-      return (
-        <div key={fTDayMonthKey} className="div_bar" style={style} title={titleContent}>
-          {widthBar > 250 ? txtContent : ''}
-        </div>
-      );
-    });
-  };
+  function loadData(selSeat){
+    console.log('loadData reservation list', selSeat);
+    const params = {
+      selSeat: selSeat,
+    };
+    const callback = function(r){
+      const rslt = r.map((val, key) => {
+        if(typeof val.startdate === 'string'){
+          val.startDate = new Date(val.startdate)
+        }
+        if(typeof val.enddate === 'string'){
+          val.endDate = new Date(val.enddate)
+        }
+        val.name = `Id${val.id}`;
+        return val;
+      });
+      // console.log('rslt', rslt);
+      setReservation(rslt);
+    }
+    utils.getReservationDb(params, callback)
+  }
 
-  const days4Month = (item) => {
-    return item.days.map((val, key) => {
-      const selDay = new Date(item.year, item.month - 1, val.day, 0, 0, 0);
-      const startDate = reservationList.find((item2) => val.dayBook.at(-1).id === item2.id).startDate;
-      const isDisabled = startDate <= currentDate;
-      const isToday = selDay.getTime() === currentDate.getTime();
-      const titleText = 'Delete booking' + (isDisabled ? ' not possible. In the past' : '');
-      return (
-        <tr key={item.month + '_' + key} {...(isToday ? { className: 'today' } : {})}>
-          <td>{val.day} {val.weekday}</td>
-          <td>
-            {barEl(val.dayBook, val.day + '_' + item.month)}
-            {isToday && <div className="today">Today</div>}
-          </td>
-          <td>
-            <button className="btn" title={titleText} onClick={(evt) => { delRow(evt, isDisabled, val); }}>
-              <FontAwesomeIcon {...(isDisabled ? { className: 'fa-disabled' } : {})} icon={faTrash} />
-            </button>
-          </td>
-        </tr>
-      );
-    });
-  };
+  function onClickRow(_id) {
+    // console.log('onClickRow', _id);
+    setChildKey(_id);
+    setCalendarActive(false);
+    setSelRow(reservationData.find(item => item.id === _id));
+  }
 
-  const tableContent = reservationData.map((val, key) => {
-    return (
-      <div key={'div_' + key} className="wrapper_table">
-        <div className="wrapper_month">{moment(val.month, 'M').format('MMMM')} {val.year}</div>
-        <Table>
-          <tbody>
-            {days4Month(val)}
-          </tbody>
-        </Table>
-      </div>
-    );
-  });
-
-  function delRow(evt, isDisabled, item) {
+  function editRow(evt, _id) {
+    // console.log('editRow', _id);
     evt.stopPropagation();
-    if (isDisabled) return;
+    setChildKey(_id);
+    setCalendarActive(true);
+    setSelRow(reservationData.find(item => item.id === _id));
+  }
 
-    setIdToDel(item.dayBook.at(-1).id);
+  function delRow(evt, _id, _startDate) {
+    // console.log('delRow', _id);
+    evt.stopPropagation();
+    if(_startDate < currentDate){
+      setShowAlert2('Not possibile to delete a book, which has been already started');
+    }else{
+      setIdToDel(_id);
+    }
+  }
+  
+  function addRow() {
+    // console.log('addRow');
+    const tomorrowAfternoon = moment(new Date()).startOf('date').add(42, 'hours').toDate();
+    const newRow = { id: null, seatId: props.selSeat, user: token.user, 
+      startDate: currentDate, endDate: tomorrowAfternoon};
+    setChildKey(null);
+    setCalendarActive(true);
+    setSelRow(newRow);
+  }
+
+  function checkIfDisabled(item){
+    let isDisabled = false;
+    if(item.username!==token.user || item.endDate < currentDate) {
+      isDisabled = true; 
+    }
+    return isDisabled
   }
 
   const handleClose = () => setIdToDel(false);
   const handleDel = () => {
     const callback = () => {
-      refresh();
+      refresh(utils.MSG.del);
       setIdToDel(null);
-    };
-    utils.delReservationDb(idToDel, callback);
-  };
+    }
 
-  function refresh() {
-    loadData();
-    setShowAlert(utils.MSG.del);
-    setTimeout(() => { setShowAlert(null); }, 2500);
+    setChildKey(null);
+    setReservation(reservationData.filter(item => item.id !== idToDel));
+    utils.delReservationDb(idToDel, callback)
   }
 
+  function refresh(msg) {
+    loadData(props.selSeat);
+    setCalendarActive(false);
+    setSelRow(null);
+    setShowAlert(msg);
+    setTimeout(()=>{setShowAlert(null);}, 2500);
+  }
+
+  function check(dateInterval, id){
+    // console.log('check in ReservationList', dateInterval, id);
+    const errorData = reservationData
+      .filter(
+        function(item){
+        return (item.id !== id && ((dateInterval[0] > item.startDate && dateInterval[0] < item.endDate)  // start inside existing dates
+          || (dateInterval[1] > item.startDate && dateInterval[1] < item.endDate) // end inside existing dates
+          || (dateInterval[0] <= item.startDate && dateInterval[1] >= item.endDate))) 
+        })
+    if(errorData.length===0)
+      return null;
+    const htmlData = errorData.map((item, key)=>{
+      const id = item.id?"(id " + item.id + ")":"";
+      const errorMsg1 = `ERROR: not possible to save selected reservation (${dateInterval[0].toLocaleDateString()} - ${dateInterval[1].toLocaleDateString()})`;
+      const errorMsg2 = `overlaps the existing one with ${id} (${item.startDate.toLocaleDateString()} - ${item.endDate.toLocaleDateString()})`;
+      return (<p key={key}>{errorMsg1}<br/>{errorMsg2}</p>)
+    });
+    return <div>{htmlData}</div>;
+  }
+
+  const tableContent = reservationData.map((val, key) => {
+    const mStartDate = moment(val.startDate);
+    const mEndDate = moment(val.endDate);
+    const isDisabled = checkIfDisabled(val);
+    return (
+      <tr key={key} title="show reservation" onClick={() => onClickRow(val.id)} {...((selRow?.id===val.id) ? {className: 'sel-tr'} : {})} >
+        <td>{val.name}</td>
+        <td>{val.username}</td>
+        <td><span>from</span>
+        <span>
+          &nbsp;{mStartDate.format('DD.MM.yyyy')}&nbsp;
+          ({mStartDate.format('HH:mm')})
+        </span>
+        <span>&nbsp;&nbsp;&nbsp;to&nbsp;&nbsp;&nbsp;</span>
+        <span>
+          &nbsp;{mEndDate.format('DD.MM.yyyy')}&nbsp;
+          ({mEndDate.format('HH:mm')})
+        </span></td>
+        {isDisabled ? 
+          <td title="Edit booking not possible" ><FontAwesomeIcon className="fa-disabled" icon={faPen} /></td>
+        :
+          <td onClick={(evt) => { editRow(evt, val.id);}}>
+            <button className="btn" title="Edit booking" >
+              <FontAwesomeIcon icon={faPen} />
+            </button>
+          </td>
+        }
+        {isDisabled ? 
+          <td><FontAwesomeIcon className="fa-disabled" icon={faTrash} /></td>
+        :
+          <td onClick={(evt) => {delRow(evt, val.id, val.startDate)}}>
+            <button className="btn" title="Delete booking"  >
+              <FontAwesomeIcon icon={faTrash} />
+            </button>
+          </td>
+        }
+      </tr>
+    )
+  })
   return (
     <ElementStyle>
-      <BModal show={!!showAlert} size="sm" centered backdrop="static">
-        <BModal.Body>{showAlert}</BModal.Body>
-        <BModal.Footer>
-          <Button variant="secondary" onClick={() => setShowAlert(null)}>Close</Button>
-        </BModal.Footer>
-      </BModal>
-
+      <Alert show={showAlert?true:false} msg={showAlert} variant="success" setShow={setShowAlert}/>
+      <Alert show={showAlert2?true:false} msg={showAlert2} variant="danger" setShow={setShowAlert2}/>
       <Modal idToDel={idToDel} handleClose={handleClose} handleDel={handleDel} />
-      {reservationData.length > 0 ? tableContent : <h2>No reservations until now!</h2>}
+      <div className='div_date_list'>
+        <p>Reservation for desk {props.selSeat}</p>
+        <div className='wrapper-scroll'>
+          {reservationData.length > 0 ?
+          <Table>
+            <tbody> 
+                {tableContent}
+            </tbody>
+          </Table>:<>No booking</>}
+        </div>
+        {props.selSeat && token.role === 'user' && <div className='wrapper-plus'>
+            <Button className='plus' type="button" onClick={() => { addRow()}}>Add <FontAwesomeIcon icon={faPlus} /></Button>
+        </div>}
+      </div>
+      {selRow && <CalendarContainer isCalendarActive={isCalendarActive} setSelRow={setSelRow} selSeat={selRow} 
+        user={token.user} key={childKey} refreshFn={refresh} msg={utils.MSG} check={check} currentDate={currentDate}/>}
     </ElementStyle>
   );
 }
 
+export default ReservationList;
