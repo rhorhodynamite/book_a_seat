720381d7849b8fda0ae1717e52594bf6c84a2401
rhorhodynamite <162373331+rhorhodynamite@users.noreply.github.com>
2024-05-24 01:05:43 +0200

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,153 +1,67 @@
 import React, { useContext, useState } from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 import AuthContext from '../context/AuthProvider';
 import styled from 'styled-components';
-import Tab from 'react-bootstrap/Tab';
-import Tabs from 'react-bootstrap/Tabs';
-import Diagram from './Diagram';
-import ReservationList from './ReservationList';
-import MyBooking from './MyBooking';
-import RoomList from './RoomList'; // Assuming RoomList is a separate component
-import SVGPlan from './SvgPlan'; // Ensure SVGPlan is imported if used
-import SVGPlanUpstairs from './SvgPlanUpstairs';
-import SVGPlanSeminar from './SvgPlanSeminar'; // Import the new SVG component
 
-const SERVER_URL = process.env.REACT_APP_SERVER_URL;
+const NavBarStyle = styled.div`
+  .navbar {
+    width: 100%;
+    background: #333; /* Darker background for contrast */
+    color: white;
+    font-family: 'Playfair Display', serif; /* Art Nouveau style font */
+    padding: 10px 0;
+  }
 
-const ElementStyle = styled.div`
-  > div {
-    width: 1000px;
+  .navbar-brand {
+    font-size: 1.5rem;
+    font-weight: bold;
+    margin-left: 20px;
   }
-  .container-fluid {
-    // background-color: #e3f2fd;
-  }
+
   .navbar-collapse {
-    justify-content: right;
+    justify-content: flex-end;
+    margin-right: 20px;
   }
-  .show .dropdown-toggle {
-    background-color: #0d6efd;
+
+  .dropdown-toggle::after {
+    display: none;
   }
 `;
 
-function NavBar() {
+const NavBar = () => {
   const { token, setToken } = useContext(AuthContext);
-  const user = <span>{token.user}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<FontAwesomeIcon icon={faUser} transform="grow-5" /></span>;
+  const user = (
+    <span>
+      {token.user}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <FontAwesomeIcon icon={faUser} transform="grow-5" />
+    </span>
+  );
 
-  const logout = function(){
-    console.log('logout');
+  const logout = () => {
     setToken(null);
   };
 
-  const [keyBooking, setKeyBooking] = useState(0);
-  const [keyDiagram, setKeyDiagram] = useState((token.role === 'admin' ? 1 : 0));
-  const [selSeat, setSelSeat] = useState(null);
-  const [keyUpstairsDiagram, setKeyUpstairsDiagram] = useState(1);
-  const [keySeminarDiagram, setKeySeminarDiagram] = useState(1);
-  const [selectedRoom, setSelectedRoom] = useState(null);
-
-  function setRoomSelectionHandler(room) {
-    setSelectedRoom(room);
-  }
-
-  function onSelectChange(tabElName) {
-    if(tabElName === 'reservation') {
-      setSelSeat(null);
-      setKeyDiagram(keyDiagram + 1);
-    } else if(tabElName === 'upstairs') {
-      setSelSeat(null);
-      setKeyUpstairsDiagram(keyUpstairsDiagram + 1);
-    } else if(tabElName === 'seminar') {
-      setSelSeat(null);
-      setKeySeminarDiagram(keySeminarDiagram + 1);
-    } else {
-      setKeyBooking(keyBooking + 1);
-    }
-  }
-
-  function setSelSeatHandler(id) {
-    setSelSeat(id); 
-  }
-
   return (
-    <ElementStyle>
-      <Navbar className='navbar navbar-light'>
+    <NavBarStyle>
+      <Navbar className="navbar navbar-light">
         <Container fluid>
           <Navbar.Brand href="#home">Book a desk!</Navbar.Brand>
-          
           <Navbar.Collapse id="navbarScroll">
             <Nav>
-              <NavDropdown title={user} id="navbarScrollingDropdown" align="end" menuVariant='#e3f2fd'>
-                <NavDropdown.Item href="#" onClick={()=>{logout();}}>Logout</NavDropdown.Item>
+              <NavDropdown title={user} id="navbarScrollingDropdown" align="end">
+                <NavDropdown.Item href="#" onClick={logout}>
+                  Logout
+                </NavDropdown.Item>
               </NavDropdown>
-              
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      
-      <Tabs onSelect={(tabElName) => onSelectChange(tabElName)}
-            defaultActiveKey={token.role === 'user' ? "booking" : "reservation"}
-            className="mb-3">
-        {token.role === 'user' && (
-          <Tab eventKey="booking" title="Bookings">
-            <div className='wrapper-dashboard'>  
-              <MyBooking username={token.user} key={keyBooking} />
-            </div>
-          </Tab>
-        )}
-        <Tab eventKey="reservation" title="First Floor">
-          <div>  
-           <h2>{token.role === 'admin' ? "ADMIN - Manage First Floor" : "First Floor Plan"}</h2>
-            {keyDiagram > 0 && (
-              <div className='wrapper-dashboard' key={'diagram_' + keyDiagram}>
-                <Diagram 
-                    apiUrl={SERVER_URL + 'api/seats'} // Same endpoint for data
-                    setSelSeat={setSelSeatHandler} 
-                    svgType="main" // Prop to control SVG display
-                />
-                <ReservationList selSeat={selSeat}/>
-              </div>
-            )}
-          </div>
-        </Tab>
-        <Tab eventKey="upstairs" title="Upstairs">
-          <div>
-            <h2>{token.role === 'admin' ? "ADMIN - Manage Upstairs Area" : "Upstairs Office Plan"}</h2>
-            {keyUpstairsDiagram > 0 && (
-              <div className='wrapper-dashboard' key={'upstairsDiagram_' + keyUpstairsDiagram}>
-                <Diagram 
-                    apiUrl={SERVER_URL + 'api/seats'} // Same endpoint for data
-                    setSelSeat={setSelSeatHandler} 
-                    svgType="upstairs" // Prop to control SVG display
-                />
-                <ReservationList selSeat={selSeat}/>
-              </div>
-            )}
-          </div>
-        </Tab>
-      <Tab eventKey="seminar" title="Meeting Raum (OG)/Telefonbox">
-          <div>
-            <h2>{token.role === 'admin' ? "ADMIN - Manage Seminar Room/Phone Booth" : "Meeting Raum (OG)/Telefonbox Plan"}</h2>
-            {keySeminarDiagram > 0 && (
-              <div className='wrapper-dashboard' key={'seminarDiagram_' + keySeminarDiagram}>
-                <Diagram 
-                    apiUrl={SERVER_URL + 'api/seats'} // Same endpoint for data
-                    setSelSeat={setSelSeatHandler} 
-                    svgType="seminar" // Prop to control SVG display
-                />
-                <ReservationList selSeat={selSeat}/>
-              </div>
-            )}
-          </div>
-        </Tab>
-      </Tabs>
-    </ElementStyle>
+    </NavBarStyle>
   );
-}
+};
 
 export default NavBar;
+
