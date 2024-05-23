5334b0a6a01e324ba3de600bc72c8400b874dca2
rhorhodynamite <162373331+rhorhodynamite@users.noreply.github.com>
2024-05-23 16:21:37 +0200

    Update Times.js

--- src/components/Times.js
+++ src/components/Times.js
@@ -1,16 +1,14 @@
-
-import React from 'react'
-import {useState} from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
 const ElementStyle = styled.div`
-  >div{
+  > div {
     text-align: right;
     position: relative;
     margin-bottom: 15px;
   }
   
-  input{
+  input {
     width: 100%;
   }
   input[type=range]::-webkit-slider-runnable-track {
@@ -23,60 +21,60 @@ const ElementStyle = styled.div`
     margin-top: -5px;
   }
 
-  output{
+  output {
     position: absolute;
     top: 18px;
     right: 0;
   }
 `;
 
-const MIN = 0;
-const MAX = 24;
-function Times(props) {
+const MIN = 8; // Starting at 08:00
+const MAX = 20; // Ending at 20:00
 
+function Times(props) {
   const startValue = props.timeInterval[0];
   const endValue = props.timeInterval[1];
-  // console.log('startValue  endValue', startValue, endValue);
 
   const onStartChange = (val) => {
-    props.setTimeOnChange([val, endValue])
+    props.setTimeOnChange([parseFloat(val), endValue]);
   };
   
   const onEndChange = (val) => {
-    props.setTimeOnChange([startValue, val])
+    props.setTimeOnChange([startValue, parseFloat(val)]);
   };
 
   function getHhMm(val) {
-    const mm = val-Math.floor(val)==0.5?'30':'00'
-    return `${Math.floor(val)}:${mm}`;
+    const hours = Math.floor(val);
+    const minutes = (val - hours) === 0.5 ? '30' : '00';
+    return `${hours.toString().padStart(2, '0')}:${minutes}`;
   }
 
-
-return (
-  <ElementStyle>
-    <div>
-      <input
-        type="range"
-        min={MIN}
-        max={MAX}
-        step={0.5}
-        onChange={(e) => onStartChange(e.target.value)}
-        value={startValue}
-      />
-      <output>{getHhMm(startValue)}</output>
-    </div>
-    <div>
-      <input
-        type="range"
-        min={MIN}
-        max={MAX}
-        step={0.5}
-        onChange={(e) => onEndChange(e.target.value)}
-        value={endValue}
-      />
-      <output>{getHhMm(endValue)}</output>
-    </div>
-  </ElementStyle>
-  )
+  return (
+    <ElementStyle>
+      <div>
+        <input
+          type="range"
+          min={MIN}
+          max={MAX}
+          step={0.5}
+          onChange={(e) => onStartChange(e.target.value)}
+          value={startValue}
+        />
+        <output>{getHhMm(startValue)}</output>
+      </div>
+      <div>
+        <input
+          type="range"
+          min={MIN}
+          max={MAX}
+          step={0.5}
+          onChange={(e) => onEndChange(e.target.value)}
+          value={endValue}
+        />
+        <output>{getHhMm(endValue)}</output>
+      </div>
+    </ElementStyle>
+  );
 }
+
 export default Times;
