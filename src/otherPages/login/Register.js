e694616a02e7b5d5cc7b61939d75a09c413bfcb9
rhorhodynamite <162373331+rhorhodynamite@users.noreply.github.com>
2024-05-23 15:43:05 +0200

    Update Register.js

--- src/otherPages/login/Register.js
+++ src/otherPages/login/Register.js
@@ -1,12 +1,24 @@
 // pages/register.js
 import React from 'react';
 import RegisterForm from '../components/RegisterForm';
+import { Container, Typography, Box } from '@mui/material';
 
 export default function RegisterPage() {
   return (
-    <div>
-      <h1>Register</h1>
-      <RegisterForm />
-    </div>
+    <Container maxWidth="sm">
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          marginTop: 8,
+        }}
+      >
+        <Typography component="h1" variant="h4">
+          Register
+        </Typography>
+        <RegisterForm />
+      </Box>
+    </Container>
   );
 }
