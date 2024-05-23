83eef4bbc0fa16a275b63c8146ba776b1c8e8300
rhorhodynamite <162373331+rhorhodynamite@users.noreply.github.com>
2024-05-24 00:52:26 +0200

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,48 +1,43 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState}  from 'react';
 import Diagram from './Diagram';
 import NavBar from './NavBar';
 import ReservationList from './ReservationList';
 import AuthContext from '../context/AuthProvider';
-import styled from 'styled-components';
 
+import styled from 'styled-components';
 const ElementStyle = styled.div`
-  position: relative;
-  background: #E7EAD4; /* Updated background color */
-  min-height: 100vh; /* Ensure it covers the full height */
-  display: flex;
-  flex-direction: column;
-  align-items: center; /* Center the items horizontally */
-  justify-content: flex-start; /* Start items from the top */
-  padding-top: 20px; /* Add some padding from the top */
-  
+  {
+    position: relative;
+    background: #E7EAD4; /* Updated background color */
+  }
+
   .h2, h2 {
     font-size: 1.5rem;
   }
 
-  .ref-wrapper {
+  .ref-wrapper{
     margin-top: 2em;
     font-size: 15px;
     span {
       font-style: italic;
+      
     }
   }
 `;
 
-const Dashboard = () => {
+export default function Dashboard() {
   const { token } = useContext(AuthContext);
   const [selSeat, setSelSeat] = useState(null);
 
-  const setSelSeatHandler = (id) => {
-    setSelSeat(id);
-  };
+  function setSelSeatHandler(id){
+    setSelSeat(id); 
+  }
 
-  return (
+  return(
     <ElementStyle>
-      <NavBar />
-      <Diagram />
-      <ReservationList />
+      <NavBar/>
+      
     </ElementStyle>
-  );
-};
+  )
+}
 
-export default Dashboard;
