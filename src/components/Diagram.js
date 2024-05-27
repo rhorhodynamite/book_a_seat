66a66b936bf1c7ddc7b39c26da2aabd5d74f772f
rhorhodynamite <162373331+rhorhodynamite@users.noreply.github.com>
2024-05-27 10:13:29 +0200

    Update Diagram.js

--- src/components/Diagram.js
+++ src/components/Diagram.js
@@ -1,9 +1,9 @@
+import React, { useState, useContext, useRef, useEffect } from 'react';
 import { useD3 } from '../use/useD3';
 import SvgPlan from './SvgPlan';
 import SeatsAndTablesClass from './SeatsAndTablesClass';
 import Popup from './Popup';
 import axios from '../api/axios';
-import React, { useState, useContext, useRef, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSave } from '@fortawesome/free-solid-svg-icons';
 import Button from 'react-bootstrap/Button';
@@ -14,8 +14,8 @@ import styled from 'styled-components';
 import SVGPlan from './SvgPlan';
 import SVGPlanUpstairs from './SvgPlanUpstairs';
 import SVGPlanSeminar from './SvgPlanSeminar';
-const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
+const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 const SVG_WIDTH = "300mm";
 const SVG_HEIGHT = "125mm";
 
@@ -69,8 +69,8 @@ function Diagram({ apiUrl = `${SERVER_URL}api/seats`, setSelSeat = () => {}, svg
   const [showAlert, setShowAlert] = useState(null);
   const chairsMng = useRef(null);
   const divStyle = { width: SVG_WIDTH, height: SVG_HEIGHT };
-  const tableWidthRef = useRef(null);  // Define ref
-  const tableHeightRef = useRef(null);  // Define ref
+  const tableWidthRef = useRef(null);
+  const tableHeightRef = useRef(null);
   const SvgComponent = svgType === "upstairs" 
     ? SVGPlanUpstairs 
     : svgType === "seminar" 
@@ -87,55 +87,52 @@ function Diagram({ apiUrl = `${SERVER_URL}api/seats`, setSelSeat = () => {}, svg
 
   useEffect(() => {
     if (chairsMng.current) {
-      chairsMng.current.tableWidth = tableWidthRef.current;  // Assign ref
-      chairsMng.current.tableHeight = tableHeightRef.current;  // Assign ref
+      chairsMng.current.tableWidth = tableWidthRef.current;
+      chairsMng.current.tableHeight = tableHeightRef.current;
     }
   }, [chairsMng]);
 
   async function loadData(svg) {
-  if (!data) {  // Only fetch if external data isn't provided
-    try {
-      const response = await axios.get(apiUrl, {
-        params: { svgType },
-        withCredentials: true,
-      });
-      const newData = response.data;
-      setData ? setData(newData, svgType) : setInternalData(newData);
-    } catch (err) {
-      console.error("ERROR loadData", err);
+    if (!data) {
+      try {
+        const response = await axios.get(apiUrl, {
+          params: { svgType },
+          withCredentials: true,
+        });
+        const newData = response.data;
+        setData ? setData(newData, svgType) : setInternalData(newData);
+      } catch (err) {
+        console.error("ERROR loadData", err);
+      }
     }
   }
-}
-
 
   function renderData(svg, dataToRender) {
-  chairsMng.current = new SeatsAndTablesClass(
-    svg, 
-    dataToRender, 
-    token.role, 
-    setSelSeat, 
-    tableWidthRef.current, 
-    tableHeightRef.current,
-    bookedSeatsForToday 
-  );
-}
-
+    chairsMng.current = new SeatsAndTablesClass(
+      svg, 
+      dataToRender, 
+      token.role, 
+      setSelSeat, 
+      tableWidthRef.current, 
+      tableHeightRef.current,
+      bookedSeatsForToday // Pass the bookedSeatsForToday prop
+    );
+  }
 
   async function save() {
-  try {
-    const params = { seats: chairsMng.current.seatData, tables: chairsMng.current.tableData };
-    const response = await axios.post(apiUrl, params, {
-      params: { svgType },
-      headers: { 'Content-Type': 'application/json' },
-      withCredentials: true,
-    });
-    setShowAlert('Row has been successfully saved!');
-    setTimeout(() => setShowAlert(null), 2500);
-  } catch (err) {
-    console.error("ERROR save diagram", err);
+    try {
+      const params = { seats: chairsMng.current.seatData, tables: chairsMng.current.tableData };
+      const response = await axios.post(apiUrl, params, {
+        params: { svgType },
+        headers: { 'Content-Type': 'application/json' },
+        withCredentials: true,
+      });
+      setShowAlert('Row has been successfully saved!');
+      setTimeout(() => setShowAlert(null), 2500);
+    } catch (err) {
+      console.error("ERROR save diagram", err);
+    }
   }
-}
-
 
   return (
     <ElementStyle>
@@ -145,7 +142,7 @@ function Diagram({ apiUrl = `${SERVER_URL}api/seats`, setSelSeat = () => {}, svg
           <Button className='save' onClick={() => chairsMng.current.addTable()}>Add a table <FontAwesomeIcon icon={faSave} /></Button>
           <div className="form-group">
             <Form.Label htmlFor="table-width">width:</Form.Label>
-            <Form.Control type="input" id="table-width" ref={tableWidthRef}/>  // Use ref
+            <Form.Control type="input" id="table-width" ref={tableWidthRef}/>  {/* Use ref */}
           </div>
           <div className="form-group">
             <Form.Label htmlFor="table-height">height:</Form.Label>
