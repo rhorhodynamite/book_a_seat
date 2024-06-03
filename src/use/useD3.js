07b89c8f8f29a2f67303659aac146167b773ae56
rhorhodynamite <162373331+rhorhodynamite@users.noreply.github.com>
2024-06-03 15:38:38 +0200

    Update useD3.js

--- src/use/useD3.js
+++ src/use/useD3.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import * as d3 from 'd3';
 
-export const useD3 = (renderChartFn, dependencies) => { // 2 arguments: 1 function + 1 array di dependencs
-    const ref = React.useRef();  // creates a plain JS object....
-    // https://www.pluralsight.com/guides/using-d3.js-inside-a-react-app
+export const useD3 = (renderChartFn, dependencies) => {
+    const ref = React.useRef();
+
     React.useEffect(() => { 
-        renderChartFn(d3.select(ref.current));  // 1 function e' anonima con argoment  => ref is svg tag, svg is object  
+        renderChartFn(d3.select(ref.current));  
         return () => {};
-      }, dependencies); // questo indica quando si verifica la condizione (in questo caso sempre uguale e quindi non fa niente)...
+      }, [renderChartFn, ...dependencies]); // Ensure dependencies are in an array literal
+
     return ref;
-} // ATTENZIONE ritorna ref 
+}
